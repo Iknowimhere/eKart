4be821ae4c671811a58b4efe959e6b9e83fd7ed3
umashankar <umashankarp33@gmail.com>
2024-01-31 14:21:28 +0530

    add registering user and login user feature

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import userRouter from '../routes/userRoutes.js'
 import {dbConfig} from '../config/dbConfig.js'
 
 dbConfig()
 
 //app instance
-let app=express()
+let app=express();
 
-app.get("/",(req,res)=>{
-    res.send("eapparel")
-})
+//middleware to process incoming json data
+app.use(express.json());
+
+app.use("/api/v1/users",userRouter);
 
 export default app;
